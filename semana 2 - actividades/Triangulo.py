c3d0cf0f53526c8a4f88b0a371abbd8ebbcf4f4d
Bryan Motta <bmottab@ulasalle.edu.pe>
2025-01-13 10:51:35 -0500

    setters and getters

--- semana 2 - actividades/Triangulo.py
+++ semana 2 - actividades/Triangulo.py
@@ -1,8 +1,15 @@
 class Triangulo:
     def __init__(self,base,altura):
-        self.base = base
-        self.altura = altura
-    
+        self.__base = base
+        self.__altura = altura
+    def get_base(self):
+        return self.__base
+    def set_base(self, base):
+        self.__base = base
+    def get_altura(self):
+        return self.__altura
+    def set_base(self, altura):
+        self.__altura = altura
     def area(self):
         self.areaR = self.base *  self.altura
 
