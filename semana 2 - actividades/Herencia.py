7e8dd6a5a559fbee72f3ac38800db37fcd795408
Bryan Motta <bmottab@ulasalle.edu.pe>
2025-01-13 11:27:28 -0500

    Si se puede heredar atributos privados de la superclase

--- semana 2 - actividades/Herencia.py
+++ semana 2 - actividades/Herencia.py
@@ -1,13 +1,13 @@
 class Superclase:
     def __init__(self, nombre):
-        self.nombre = nombre
+        self.__nombre = nombre
 
 class Subclase(Superclase):
     def __init__(self, nombre, apellido):
         super().__init__(nombre)
         self.__apellido = apellido
     def saludar(self):
-        print('hola'+str(self.nombre)+str(self.__apellido))
+        print('hola'+str(self._Superclase__nombre)+str(self.__apellido))
 
 persona = Subclase('bryan','Motta')
 persona.saludar()
